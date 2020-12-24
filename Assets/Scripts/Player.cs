7d1d6933c727729d8c5cc954eb261bb344a7bd4d
Nikita <nikitadedov66@gmail.com>
2020-12-24 19:53:04 +0200

    correction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,31 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Utilities;
+
 
 public class Player : MonoBehaviour {
+
     public static Vector2 currentPos;
 
     Vector2 direction;
     Vector2 point1;
     Vector2 point2;
+
     float angle;
+
     [SerializeField]
     Text text;
 
     [SerializeField]
     LineRenderer line;
 
-    [SerializeField]
-    int counter;
+    public bool inMove = false;
 
     private void Start() {
-        Debug.Log("qq");
-        currentPos = Vector2.one;
-
+        currentPos = new Vector2(5,5);
     }
 
-    void Update() {
-        //currentPos = GridGeneration.currentPos;
+    void Update() {      
         transform.position = Vector2.Lerp(transform.position, currentPos, 0.2f);
         Move();
     }
@@ -40,64 +41,28 @@ public class Player : MonoBehaviour {
         if (Input.GetMouseButton(0)) {
             point2 = Input.mousePosition;
             direction = point2 - point1;
-            angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + 90;         
-        }
-        if (Input.GetMouseButtonUp(0)) {
-            GetDirectionFromAngle(angle);
+            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            var steps = GetStepsToWall(GetDirectionFromAngle(angle));
-            Debug.Log(steps);
-            currentPos += steps;
-            
-        }
-    }
-
-    public Vector2 GetDirectionFromAngle(float angle) {
-        switch (angle) {
-            case float n when (n < 135 && n > 45):
-                text.text = "Up";
-                return Vector2.up;
-            case float n when (n > 135 && n < 225):
-                text.text = "Right";
-                return Vector2.right;
-            case float n when (n < -45 || n > 225):
-                text.text = "Down";
-                return Vector2.down;
-            case float n when (n < 45 && n > -45):
-                text.text = "Left";
-                return Vector2.left;
-            default:
-                text.text = "Nana";
-                return Vector2.zero;
-        }
-    }
-
-    private Vector2 GetStepsToWall(Vector2 direction) {
-        var startPoint = currentPos + direction;
-  
-        counter = 0;
-        for (int y = (int)startPoint.y; y < GridGeneration.gridSizeY; y++) {
-
-            for (int x = (int)startPoint.x; x < GridGeneration.gridSizeX; x++) {
-                var cellToCheck = GridGeneration.cells[GridGeneration.GetGridIndexOfCell(startPoint)];
-                if (cellToCheck.isWalkable == false) return direction * counter ;
-                
-                startPoint += direction;
-                counter++;            
+            if (angle < 0) {
+                angle = 360 - Mathf.Abs(angle);
             }
-          
         }
-        Debug.Log(counter);
-        return direction * counter;
+        if (Input.GetMouseButtonUp(0)) {        
+            var steps = Utils.GetStepsFromToWall(currentPos,Utils.GetDirectionFromAngle(angle));
+           // Debug.Log(steps);
+            currentPos += steps;
+        }
     }
 
 
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.transform.tag == "Wall") {
+            Debug.Log("Bum");
+        }
+        
+    }
 
-
-
-    private void OnDrawGizmos() {
-        Gizmos.DrawWireSphere(point1, 1);
-        Gizmos.DrawWireSphere(point2, 1);
+    private void OnCollisionEnter2D(Collision2D collision) {
     }
 
 
